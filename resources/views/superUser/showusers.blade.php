44d8542ca812f505c201434fdbed2300e0d6e96c
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2022-12-01 00:45:17 +0100

    [FRONTEND #4] Form pour ajouter users et Table Affichage Avec la fonction js pour poster les infos

---
+++ resources/views/superUser/showusers.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content_page')
+    <!-- Show All Users Start -->
+    <div class="container-fluid pt-4 px-4">
+        <div class="bg-light text-center rounded p-4">
+            <div class="d-flex align-items-center justify-content-between mb-4">
+                <h6 class="mb-0">Tous Les Utilisateurs</h6>
+                <a href="">Show All</a>
+            </div>
+            <div class="table-responsive">
+                <table class="table text-start align-middle table-bordered table-hover mb-0" id="tablebenificiere">
+                    <thead>
+                        <tr class="text-dark">
+                            <th scope="col"><input class="form-check-input" type="checkbox"></th>
+                            <th scope="col">Nom complet</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Adresse</th>
+                            <th scope="col">CIN</th>
+                            <th scope="col">N° de telephone</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- Show All Users End -->
+@endsection
