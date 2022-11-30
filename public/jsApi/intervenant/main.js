f572bd3d77927ae2c78f22a136d1de67a17cf87e
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2022-11-30 23:28:57 +0100

    [FRONTEND #3] Ajouter une fonction js pour récuperer data

---
+++ public/jsApi/intervenant/main.js
@@ -0,0 +1,24 @@
+const baseUrl = "https://www.breakingbadapi.com/api/"
+function getData(endUrl) {
+    $.ajax({
+        type: "GET",
+        url: baseUrl + endUrl,
+        dataType: "json",
+        success: (response) => {
+            var tbodyData = ""
+            $.each(response, function (key, val) {
+                tbodyData += `<tr>
+                    <td>${val.name}</td>
+                    <td>${val.name}</td>
+                    <td>${val.name}</td>
+                    <td>${val.name}</td>
+                    <td>${val.name}</td>
+                    <td>${val.name}</td>
+                </tr>`
+            });    
+            $('#tablebenificiere tbody').html(tbodyData);
+        }
+    })
+}
+
+getData("characters")
