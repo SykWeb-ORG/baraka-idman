f8f03cbf3edc6793f5387a41baa9eac7d1c4f6f7
Ayoub Khyat <ayoubkhyat@gmail.com>
2022-11-30 21:03:16 +0100

    add permission page

---
+++ resources/views/permission.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content_page')
+    <div class="container-fluid pt-4 px-4">
+        <div class="d-flex align-items-center justify-content-between mb-4">
+            <h6 class="mb-0">Add Permission to role</h6>
+        </div>
+        <div class="div_perm">
+            <select name="" id="cusSelectbox" class="cusSelectbox">
+                <option value="Select">Select</option>
+                <option value="India">India</option>
+                <option value="Nepal">Nepal</option>
+                <option value="Bangladesh">Bangladesh</option>
+                <option value="Sri Lanka">Sri Lanka</option>
+            </select>
+            <fieldset class="">
+                <legend>Permissions</legend>
+                <div class="permissions_check">
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                    <input type="checkbox" name="" id=""><label for="">Permission1</label>
+                </div>
+            </fieldset>
+        </div>
+    </div>
+@endsection
