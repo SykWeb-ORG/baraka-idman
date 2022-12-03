35d9653d94d458a333c7f7bc14b6ed8b6433eff9
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2022-12-03 21:53:43 +0100

    [FRONTEND #6 - PART 3] Modifier le main.js de bénéficiaire pour les récupere

--- public/jsApi/intervenant/main.js
+++ public/jsApi/intervenant/main.js
@@ -1,30 +1,28 @@
-$.ajaxSetup({
-    headers:{
-        'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
-    }
-})
+//Declaration DOM & Variables
+const url = "http://127.0.0.1:8000/"
 
-const baseUrl = "https://www.breakingbadapi.com/api/"
-function getData(endUrl) {
-    $.ajax({
-        type: "GET",
-        url: baseUrl + endUrl,
-        dataType: "json",
-        success: (response) => {
-            var tbodyData = ""
-            $.each(response, function (key, val) {
-                tbodyData += `<tr>
-                    <td>${val.name}</td>
-                    <td>${val.name}</td>
-                    <td>${val.name}</td>
-                    <td>${val.name}</td>
-                    <td>${val.name}</td>
-                    <td>${val.name}</td>
-                </tr>`
-            });    
-            $('#tablebenificiere tbody').html(tbodyData);
-        }
-    })
+//Functions
+function getAllBenefaicaires(benefs) {
+    var tbodyData = "";
+    $.each(benefs, function (key, benef) {
+        tbodyData += `<tr>
+            <td><input class="form-check-input" type="checkbox"></td>
+            <td>${benef.first_name}</td>
+            <td>${benef.last_name}</td>
+            <td>${benef.cin}</td>
+            <td>${benef.birthday_date}</td>
+            <td>${benef.phone_number}</td>
+            <td>${benef.email}</td>
+            <td><button type="button" class="btn btn-sm btn-sm-square btn-primary m-2"><i class="fas fa-user-edit"></i></button></td>
+            <td><button type="button" class="btn btn-sm btn-sm-square btn-primary m-2"><i class="fas fa-user-minus"></i></button></td>
+            <td><button type="button" class="btn btn-sm btn-sm-square btn-primary m-2"><i class="fas fa-user-tag"></i></button></td>
+        </tr>`
+    });    
+    $('#tablebenificiere tbody').html(tbodyData);
 }
 
-getData("characters")
+
+//GET - Read All Users
+fetch(url)
+    .then(response => response.json())
+    .then(data => getAllBenefaicaire(data))
