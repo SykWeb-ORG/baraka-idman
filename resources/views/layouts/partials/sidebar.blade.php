8bae0586225a73f4af3408e02bb38d9a28791768
Ayoub Khyat <ayoubkhyat@gmail.com>
2022-12-05 17:08:17 +0100

    Modifier les icons dans sidebar et topbar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar Start -->
 <div class="sidebar pe-4 pb-3">
     <nav class="navbar bg-light navbar-light">
-        <a href="index.html" class="navbar-brand mx-4 mb-3">
+        <a href="#" class="navbar-brand mx-4 mb-3">
             <h4 class="text-primary"><img src={{asset("images/LOGO-DROGUES.png")}} alt="" class="logo_sidebar"></h4>
         </a>
         {{-- <div class="d-flex align-items-center ms-4 mb-4">
@@ -15,10 +15,10 @@
             </div>
         </div> --}}
         <div class="navbar-nav w-100">
-            <a href="index.html" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+            <a href="index.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Dashboard</a>
             @canany(['create', 'viewAny'], App\Models\User::class)
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Utilisateurs</a>
+                    <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown"><i class="fas fa-users me-2"></i>Utilisateurs</a>
                     <div class="dropdown-menu bg-transparent border-0">
                         @can('create', App\Models\User::class)
                             <a href="{{ route('new-user-form') }}" class="dropdown-item">Ajouter</a>
@@ -31,7 +31,7 @@
             @endcanany
             @canany(['create', 'viewAny'], App\Models\Beneficiaire::class)
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Bénéficiaires</a>
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-user-shield me-2"></i>Bénéficiaires</a>
                     <div class="dropdown-menu bg-transparent border-0">
                         @can('create', App\Models\Beneficiaire::class)
                             <a href="{{ route('new-user-form') }}" class="dropdown-item">Ajouter</a>
@@ -43,9 +43,9 @@
                 </div>
             @endcanany
             @can('roles-permissions-ability')
-                <a href="{{ route('roles-permissions') }}" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Gérer les roles</a>
+                <a href="{{ route('roles-permissions') }}" class="nav-item nav-link"><i class="fas fa-tasks me-2"></i>Gérer les rôles</a>
             @endcan
-            <a href="{{ route('logout') }}" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Quitter</a>
+            <a href="{{ route('logout') }}" class="nav-item nav-link"><i class="fas fa-sign-out-alt me-2"></i>Quitter</a>
         </div>
     </nav>
 </div>
