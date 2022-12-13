d2fd3feb238285b9c2e1d847f29754117fbae66f
Khalid <ramdan.khalid1234>
2022-12-13 12:30:10 +0100

    add zone | zone request

---
+++ resources/js/zone/add-zone.js
@@ -0,0 +1,23 @@
+require('./bootstrap');
+const baseUrl = "http://localhost:8000/";
+$(document).ready(function(){
+    axios.get('/sanctum/csrf-cookie').then(response => {
+        console.log(response);
+    });
+    let btnAddZone = document.getElementById('btnAddZone');
+    btnAddZone.addEventListener('click', function(e){
+        debugger;
+        let endUrl = "zones";
+        axios.post(baseUrl + endUrl, {
+            zone_nom: document.getElementById('zone-nom').value,
+        }).then((response) => {
+            console.log(response);
+            response = response.data;
+            if (response.result) {
+                alert(response.msg);
+            } else {
+                alert(response.msg);
+            }
+        });
+    })
+})
