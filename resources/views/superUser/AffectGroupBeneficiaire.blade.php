17c2688dbebb39ef97970d9b5868461c831ba7b8
Ayoub Khyat <ayoubkhyat@gmail.com>
2023-01-22 20:48:19 +0100

    Affecter groupe a beneficiaire

---
+++ resources/views/superUser/AffectGroupBeneficiaire.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title')
+Gestion des roles avec les services
+@endsection
+@section('content_page')
+    <div class="container-fluid pt-4 px-4">
+        <div class="d-flex align-items-center justify-content-between mb-4">
+            <h6 class="mb-0">Add Group to Beneficaire</h6>
+        </div>
+        <div class="div_perm">
+            <select name="role" id="role" class="cusSelectbox">
+                <option value="Select">Atelier</option>
+                <option value="India">India</option>
+                <option value="Nepal">Nepal</option>
+                <option value="Bangladesh">Bangladesh</option>
+                <option value="Sri Lanka">Sri Lanka</option>
+            </select>
+            <fieldset class="">
+                <legend>Group</legend>
+                <div class="permissions_check" id="group_check">
+                </div>
+            </fieldset>
+            <div>
+                <button id="btnMatchGroupBenef" class="btn btn-primary">Attacher</button>
+            </div>
+        </div>
+    </div>
+@endsection
