41bd624c219591ca75879e54db9682e2581202ff
Soufiane el kaabaoui <sousou.kaa20@gmail.com>
2023-02-06 14:22:38 +0100

    show all zones by JS API

--- resources/views/interTerrain/zoneintervenance.blade.php
+++ resources/views/interTerrain/zoneintervenance.blade.php
@@ -19,22 +19,64 @@
                             <th scope="col" colspan="2" class="actions">Action</th>
                         </tr>
                     </thead>
-                    <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mhamid</td>
-                                <td class="actionU"><a href='' class="btn btn-sm btn-sm-square btn-primary m-2"><i class="fas fa-user-edit"></i></a></td>
-                                <td class="actionD">
-                                        <button type="submit" class="btn btn-sm btn-sm-square btn-primary m-2"><i class="fas fa-user-minus"></i></button>
-                                </td>
-                                <td class="actionM">
-                                    <button type="submit" class="btn btn-sm btn-sm-square btn-primary m-2"><i class="fas fa-ellipsis-h"></i></button>
-                                </td>
-                            </tr>
+                    <tbody id="tbl_zone">
+                            
                     </tbody>
                 </table>
+                <!--Edit Zone-->
+                <!-- Modal -->
+                <div class="modal fade" id="modal_EditZone" data-bs-backdrop="static" data-bs-keyboard="false"
+                tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Modifier zone</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                </button>
+                            </div>
+                            <div class="modal-bodyEdit">
+                                <form class="form-user" action="" method="POST">
+                                    <div class="mb-3">
+                                        <label for="nom-zone" class="form-label">Nom de la zone</label>
+                                        <input type="text" name="nom-zone" class="form-control" id="nom-zone">
+                                    </div>
+                                    <div class="mb-3">
+                                        <button id="btn-edit-zone" class="btn btn-primary">Modifier</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--End Modal-->
+                <!--End Edit Zone-->
+                <!--Delete Zone-->
+                <!-- Modal -->
+                <div class="modal fade" id="modal_DeleteZone" data-bs-backdrop="static" data-bs-keyboard="false"
+                    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Confirmer Suppression</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                </button>
+                            </div>
+                            <div class="modal-bodyEdit">
+                                <div class="mb-3 mt-3">
+                                    <button id="btn-delete-zone" class="btn btn-secondary">Oui</button>
+                                    <button class="btn btn-primary">Non</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--End Modal-->
+                <!--End Delete Zone-->
             </div>
         </div>
     </div>
     <!-- Recent Sales End -->
+@endsection
+@section('custom_scripts')
+    <script src="{{ asset('jsApi/zone/all-zones.js') }}"></script>
 @endsection
