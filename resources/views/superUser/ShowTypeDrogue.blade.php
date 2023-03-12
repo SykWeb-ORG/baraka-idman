3500a5c346df9952207123224f72ed514a5175ca
Soufiane el kaabaoui <sousou.kaa20@gmail.com>
2023-03-12 16:16:55 +0100

    show all drogue types with JS API

--- resources/views/superUser/ShowTypeDrogue.blade.php
+++ resources/views/superUser/ShowTypeDrogue.blade.php
@@ -18,15 +18,10 @@
                             <th colspan="2" class="actions">Action</th>
                         </tr>
                     </thead>
-                    <tbody id="tbl_type-drogue">
+                    <tbody id="tbl_drogue_types">
                     </tbody>
                 </table>
                 <!--Edit Type De Drogue-->
-                <button
-                      class="btn btn-sm btn-sm-square btn-primary m-2"type="button"
-                      data-bs-toggle="modal" data-bs-target="#modal_EditTypeDrogue">
-                      <i class="fas fa-edit"></i>
-                  </button>
                 <!-- Modal -->
                 <div class="modal fade" id="modal_EditTypeDrogue" data-bs-backdrop="static" data-bs-keyboard="false"
                     tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -40,11 +35,11 @@
                             <div class="modal-bodyEdit">
                                 <form class="form-user" action="" method="POST">
                                     <div class="mb-3">
-                                        <label for="nom-type-drogue" class="form-label">Nom du Type De Drogue</label>
-                                        <input type="text" name="nom-type-drogue" class="form-control" id="nom-type-drogue">
+                                        <label for="nom-drogue-type" class="form-label">Nom du Type De Drogue</label>
+                                        <input type="text" name="nom-drogue-type" class="form-control" id="nom-drogue-type">
                                     </div>
                                     <div class="mb-3">
-                                        <button id="btn-edit-type-drogue" class="btn btn-primary">Modifier</button>
+                                        <button id="btn-edit-drogue-type" class="btn btn-primary">Modifier</button>
                                     </div>
                                 </form>
                             </div>
@@ -54,10 +49,6 @@
                 <!--End Modal-->
                 <!--End Edit Type De Drogue-->
                 <!--Delete Type De Drogue-->
-                <button class="btn btn-sm btn-sm-square btn-primary m-2"type="button" data-bs-toggle="modal"
-                    data-bs-target="#modal_DeleteTypeDrogue">
-                    <i class="fas fa-trash"></i>
-                </button>
                 <!-- Modal -->
                 <div class="modal fade" id="modal_DeleteTypeDrogue" data-bs-backdrop="static" data-bs-keyboard="false"
                     tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">>
@@ -70,8 +61,8 @@
                             </div>
                             <div class="modal-bodyEdit">
                                     <div class="mb-3 mt-3">
-                                        <button id="btn-delete-type-drogue" class="btn btn-secondary">Oui</button>
-                                        <button id="btn-delete-type-drogue" class="btn btn-primary">Non</button>
+                                        <button id="btn-delete-drogue-type" class="btn btn-secondary">Oui</button>
+                                        <button class="btn btn-primary">Non</button>
                                     </div>
                             </div>
                         </div>
@@ -82,4 +73,7 @@
             </div>
         </div>
     </div>
+@endsection
+@section('custom_scripts')
+    <script src="{{ asset('jsApi/drogueType/all-drogue-types.js') }}"></script>
 @endsection
