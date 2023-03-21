ff9e9d51a86919f22c0c13233ad4bb98b3f1e18a
Ayoub Khyat <ayoubkhyat@gmail.com>
2023-03-21 15:52:28 +0000

    Afficher l'image en plein ecran

--- public/js/main.js
+++ public/js/main.js
@@ -112,3 +112,31 @@ function SelectDB(){
     }
 
 }
+$(document).ready(function () {
+    $('.support').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+    $('#splink').on('click', function (e) {
+        if (window.innerWidth < 769)
+            $("#menu").fadeIn(500);
+        $(".fa-angle-down").css({ "transition": "0.9s", "transform": "rotateX(180deg)" })
+    })
+
+    $('#splink').on('mouseenter', function (e) {
+        if (window.innerWidth > 769) {
+            e.preventdefault = false;
+            $("#menu").fadeIn(500);
+            $(".fa-angle-down").css({ "transition": "0.9s", "transform": "rotateX(180deg)" })
+        }
+    })
+    $('#splink').on('mouseleave', function () {
+        if (window.innerWidth > 1) {
+            $("#menu").fadeOut();
+            $(".fa-angle-down").css({ "transition": "0.9s", "transform": "rotateX(-180deg)" })
+        }
+    })
+});
