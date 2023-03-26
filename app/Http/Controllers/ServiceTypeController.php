6ac201d0856b800c373c3970a4224fde7a90b642
Soufiane el kaabaoui <sousou.kaa20@gmail.com>
2023-03-26 13:27:42 +0100

    add service type

---
+++ app/Http/Controllers/ServiceTypeController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ServiceTypeRequest;
+use App\Models\ServiceType;
+use Illuminate\Http\Request;
+
+class ServiceTypeController extends Controller
+{
+    public function __construct() {
+        $this->authorizeResource(ServiceType::class, 'serviceType');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ServiceTypeRequest $request)
+    {
+        $service_type = new ServiceType;
+        $service_type->service_type_nom = $request->service_type_nom;
+        if ($service_type->save()) {
+            $result = $service_type;
+            $status = 200;
+            $msg = "Type de service ajouté avec success.";
+        }else {
+            $result = null;
+            $status = 500;
+            $msg = "Probléme au serveur.";
+        }
+        return response()->json(
+            [
+                'result' => $result,
+                'msg' => $msg,
+                'status' => $status,
+            ],
+            $status
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ServiceType  $serviceType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ServiceType $serviceType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ServiceType  $serviceType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ServiceType $serviceType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ServiceType  $serviceType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ServiceType $serviceType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ServiceType  $serviceType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ServiceType $serviceType)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\PartenaireController;
 use App\Http\Controllers\ParticipantController;
 use App\Http\Controllers\ProgrammeController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ServiceTypeController;
 use App\Http\Controllers\SocialeVisiteController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ZoneController;
@@ -788,6 +789,16 @@ Route::middleware('auth:sanctum')->group(function () {
                 [
                     'result' => null,
                     'msg' => 'Pas de visite juridique',
+                ],
+                404
+            );
+        });
+    Route::resource('serviceTypes', ServiceTypeController::class)
+        ->missing(function (Request $request) {
+            return response()->json(
+                [
+                    'result' => null,
+                    'msg' => 'Pas de type de service',
                 ],
                 404
             );
