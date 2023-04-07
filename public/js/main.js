9df9ab4b31fb21f8d8ff402b65801c1d3dca7153
Ayoub Khyat <ayoubkhyat@gmail.com>
2023-04-07 21:06:52 +0000

    Add progress bar in show Project with modal

--- public/js/main.js
+++ public/js/main.js
@@ -150,3 +150,22 @@ $(document).ready(function() {
     LoaderDiv.delay(delayTime).fadeOut();
     MainDiv.delay(delayTime).fadeIn();
   });
+/*Progress bar*/
+let circularProgress = document.querySelector(".circular-progress"),
+    progressValue = document.querySelector(".progress-value");
+
+let progressStartValue = 0,    
+    progressEndValue = 90,    
+    speed = 50;
+    
+let progress = setInterval(() => {
+    progressStartValue++;
+
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(var(--primary) ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+    if(progressStartValue == progressEndValue){
+        clearInterval(progress);
+    }    
+}, speed);
+ 
